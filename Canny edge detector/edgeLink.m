23f114ebd4c141309129476f5c2036f734d06133
Grishma Jena <GJena@users.noreply.github.com>
2016-01-11 23:58:00 -0500

    Update edgeLink.m

--- Canny edge detector/edgeLink.m
+++ Canny edge detector/edgeLink.m
@@ -3,7 +3,7 @@ E=M;
 med_grad=median(J(:));
 max_val=max(J(:));
 
-%setting thresholds for hystersis
+% Setting global thresholds for hystersis
 if med_grad>5
     low_thres=1.5*med_grad;
     high_thres=1.66*med_grad;
@@ -16,16 +16,17 @@ else
     end
 end
 
-
-E(J < low_thres)=0; %discarding pixels lower than low threshold
-E(J >= high_thres)=1; %edge points
+% Discarding pixels lower than low threshold
+E(J < low_thres)=0; 
 M(J < low_thres)=0;
+% Edge points
+E(J >= high_thres)=1;
 M(J >= high_thres)=1;
 
 figure;
 imshow(E);
 
-%checking for weak edge points
+% Checking for weak edge points
 for i=2:size(theta1,1)-1
     for j=2:size(theta1,2)-1
       if(M(i,j)==1)
